35a35651d2b6f4ac1a4ed44af8963d584e44283f
Dement1cs <93581822+Dement1cs@users.noreply.github.com>
2022-12-15 21:27:00 +0000

    Add files via upload

---
+++ PROJECT/script.js
@@ -0,0 +1,47 @@
+function hide(){
+    document.getElementById('dima').style.display="hidden"
+    document.getElementById('dima').innerHTML="Start learning in a comfortable environment for you. Online learning is more flexible and convenient. You have the opportunity to study at almost any time, at your own pace"
+    document.getElementById('dima').style.fontSize="30"
+    document.getElementById('dima').style.width="700"
+    document.getElementById('dima').style.height="467"
+    document.getElementById('dima').style.textAlign="left"
+    document.getElementById('dima').style.backgroundColor="#fda93b"
+    document.getElementById('dima').style.color="#ffffff"
+    document.getElementById('dima').style.paddingLeft="10px"
+}
+
+function hide2(){
+    document.getElementById('dima2').style.display="hidden"
+    document.getElementById('dima2').innerHTML="Online learning can allow you to travel or fly away on vacation. Since you can study from anywhere, the main thing is to have access to the Internet."
+    document.getElementById('dima2').style.fontSize="30"
+    document.getElementById('dima2').style.width="700"
+    document.getElementById('dima2').style.height="467"
+    document.getElementById('dima2').style.textAlign="left"
+    document.getElementById('dima2').style.backgroundColor="#fda93b"
+    document.getElementById('dima2').style.color="#ffffff"
+    document.getElementById('dima2').style.paddingLeft="10px"
+}
+
+function hide3(){
+    document.getElementById('dima3').style.fontSize="30"
+    document.getElementById('dima3').style.display="hidden"
+    document.getElementById('dima3').innerHTML="Teamwork. It is difficult to call teamwork a plus of online learning, since work is more productive when meeting. But perhaps introverts will find it convenient to work in a team online."
+    document.getElementById('dima3').style.width="700"
+    document.getElementById('dima3').style.textAlign="left"
+    document.getElementById('dima3').style.height="467"
+    document.getElementById('dima3').style.backgroundColor="#fda93b"
+    document.getElementById('dima3').style.color="#ffffff"
+    document.getElementById('dima3').style.paddingLeft="10px"
+}
+
+function hide4(){
+    document.getElementById('dima4').style.fontSize="30"
+    document.getElementById('dima4').style.display="hidden"
+    document.getElementById('dima4').innerHTML="for those who like to eat online, studying is a very big plus, since no one will reproach you for what you eat in front of the computer (but only if the webcam is turned off)"
+    document.getElementById('dima4').style.width="700"
+    document.getElementById('dima4').style.textAlign="left"
+    document.getElementById('dima4').style.height="467"
+    document.getElementById('dima4').style.backgroundColor="#fda93b"
+    document.getElementById('dima4').style.color="#ffffff"
+    document.getElementById('dima4').style.paddingLeft="10px"
+}
